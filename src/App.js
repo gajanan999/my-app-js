5a004634da89042c1a198d357dd36e961d5f2a1e
TechGrowth22 <tech.growth22@gmail.com>
2023-01-03 19:13:43 +0100

    Adde3d a csp changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
+import React  from 'react';
+import Moment from 'react-moment';
+
 import UITheme from './components/UITheme';
+import { Grid } from 'gridjs-react';
 
 function App() {
+
+  const dateToFormat = '1976-04-19T12:59-0500';
 
   return (
     <div className="App">
@@ -12,6 +18,24 @@ function App() {
           Hello world
       </div>
       </header>
+
+      <Grid
+          data={[
+            ['John', 'john@example.com'],
+            ['Mike', 'mike@gmail.com']
+          ]}
+          columns={['Name', 'Email']}
+          search={true}
+          pagination={{
+            enabled: true,
+            limit: 1,
+          }}
+      />
+
+      {
+        
+        <Moment>{dateToFormat}</Moment>
+      }
     </div>
   );
 }
